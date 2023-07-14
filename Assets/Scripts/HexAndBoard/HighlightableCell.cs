1f5510487a524977bf8a4dc18e4609c91f5a6a40
dancingWithWolves <dimikson4@gmail.com>
2023-07-15 01:01:24 +0300

    Подсветка клеток в зависимости от абабусов на них

--- Assets/Scripts/HexAndBoard/HighlightableCell.cs
+++ Assets/Scripts/HexAndBoard/HighlightableCell.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class HighlightableCell : Highlightable
 {
     public Transform transform_;
+    private HexCell hex_cell;
     private Renderer renderer_; 
+
     public Material highlighted_material;
     private Material default_material;
     public float highlighted_y_offset;
+
     void Awake() {
         renderer_ = GetComponentInChildren<Renderer>();
+        hex_cell = GetComponentInChildren<HexCell>();
         default_material = renderer_.material;
     }
-    override public void SetHighlighted() {
+    override public void SetHighlighted(GayManager gm) {
         renderer_.material = highlighted_material;
-        transform_.position = new Vector3(transform_.position.x, transform_.position.y + highlighted_y_offset, transform_.position.z);
+
+        HexCoordinates hc = hex_cell.hex_coordinates;
+        List<Abobus> abobi_on_this_cell = gm.GetListAbobiByHexCoordinates(hc);
+        bool has_enemy = false;
+
+        foreach (Abobus abobus in abobi_on_this_cell) {
+            if (abobus.team != gm.chosen_abobus.team) {
+                Debug.Log("HOHOL DETECTED");
+                has_enemy = true;
+                break;
+            }
+        }
+        if (has_enemy) {
+            renderer_.material.color = Color.red;
+        }
+
+        transform_.position += new Vector3(0, highlighted_y_offset, 0);
     }
     override public void SetDefault() {
         renderer_.material = default_material;
-        transform_.position = new Vector3(transform_.position.x, transform_.position.y - highlighted_y_offset, transform_.position.z);
-    }
+        transform_.position += new Vector3(0, -highlighted_y_offset, 0);}
 }
--- Assets/Scripts/HighlightableI.cs
+++ Assets/Scripts/HighlightableI.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 public abstract class Highlightable: MonoBehaviour
 {
     public bool highlighted;
-    public void SwitchHighlight()
+    public void SwitchHighlight(GayManager gm)
 	{
 		highlighted = !highlighted;
 		if (highlighted) {
-            SetHighlighted();
+            SetHighlighted(gm);
 		} else {
             SetDefault();
 		}
 	}
 
-    public abstract void SetHighlighted();
+    public abstract void SetHighlighted(GayManager gm);
     public abstract void SetDefault();
 
 }
