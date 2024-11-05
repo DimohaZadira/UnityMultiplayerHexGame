09c7e5bb90f409f88dbe6a886745662a58130cc1
Kabanya <evonirazh@mail.ru>
2024-11-05 14:50:08 +0300

    медвер с багами (но с комментами 31.10 встречи)

--- Assets/Scripts/Action/AbobiSkill/MedverSkill.cs
+++ Assets/Scripts/Action/AbobiSkill/MedverSkill.cs
@@ -1,35 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MedverSkill : IAction
 {
-    private Abobus abobus1; // звероник которого выбираем
-    private Abobus abobus2; // звероник на которого тыкаем
-    private HexCell cell1; //хекс 1-ого звероника
-    private HexCell cell2; //хекс 2-ого звероника
+    private HexCell applied_to; // звероник которого выбираем
+    private Abobus initialAbobus;
+    private Abobus targetAbobus;
     private GameManager game_manager;
-    public MedverSkill(Abobus abobus2, Abobus abobus1)
+    public MedverSkill(HexCell applied_to, Abobus initialAbobus)
     {
-        this.abobus1 = abobus1;
-        this.abobus2 = abobus2;
-
-        cell1 = abobus1.cell;
-        cell2 = abobus2.cell;
-
+        this.applied_to = applied_to;
+        this.initialAbobus = initialAbobus;
         game_manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
 
     public HexCell AppliedTo
     {
-        get => cell1;
-        set => cell1 = value;
-    }
-
-    public HexCell AppliedFrom
-    {
-        get => cell2;
-        set => cell2 = value;
+        get => applied_to;
+        set => applied_to = value;
     }
 
 
@@ -41,21 +31,29 @@ public class MedverSkill : IAction
     public void Invoke()
     {
         Debug.Log("Medver invokes skill");
+
         if (game_manager.selected_abobus == null) {
-            SelectAbobus select_abobus = new SelectAbobus(abobus1.cell, abobus1);
+            SelectAbobus select_abobus = new SelectAbobus(applied_to, game_manager.GetAbobusByHexCoordinates(applied_to.hex_coordinates));
             select_abobus.Invoke();
         }
-        List<HexCell> skill_turns = abobus1.GetPossibleSkillTurns(cell1);
-        foreach (HexCell cell in skill_turns) 
+
+        Abobus initialAbobus = game_manager.GetAbobusByHexCoordinates(applied_to.hex_coordinates);
+
+        List<HexCell> skillTurns = initialAbobus.GetPossibleSkillTurns(initialAbobus.cell);
+
+        foreach (HexCell cell in skillTurns)
         {
-            cell.GetComponent<HighlightableCell>().SetState(HighlightableCell.State.highlighted_yellow);
-            cell.actions.AddLast(new Swap(abobus1, abobus2));
-            cell.actions.AddLast(new SimpleUnhighlight(abobus1.cell, skill_turns));
-            cell.actions.AddLast(new UnselectAbobus(cell, abobus1));
+            cell.GetComponent<HighlightableCell>().SetState(HighlightableCell.State.highlighted_blue);
+
+            Abobus targetAbobus = game_manager.GetAbobusByHexCoordinates(cell.hex_coordinates);
+            
+            cell.actions.AddFirst(new Swap(cell, initialAbobus, targetAbobus));
+            cell.actions.AddLast(new SimpleUnhighlight(applied_to, skillTurns));
+            cell.actions.AddLast(new UnselectAbobus(cell, initialAbobus));
             cell.actions.AddLast(new EndTurn(cell));
+
+            initialAbobus.cell.actions.AddLast(new ClearActions<IAction>(initialAbobus.cell, skillTurns));
+            
         }
-        abobus1.cell.actions.AddLast(new SimpleUnhighlight(abobus1.cell, skill_turns));
-        abobus1.cell.actions.AddLast(new ClearActions<IAction>(abobus1.cell, skill_turns));
-        abobus1.cell.actions.AddLast(new ReturnHighlights(abobus1.cell, abobus1));
     }
 }
--- Assets/Scripts/Action/Swap.cs
+++ Assets/Scripts/Action/Swap.cs
@@ -6,40 +6,32 @@ using UnityEngine;
 public class Swap : IAction
 {
     private Abobus abobus1; // звероник которого выбираем
-    private Abobus abobus2; // звероник на которого тыкаем
-    private HexCell cell1; //хекс 1-ого звероника
-    private HexCell cell2; //хекс 2-ого звероника
+    private Abobus abobus2;
+    private HexCell applied_to; //хекс 1-ого звероника
     private GameManager game_manager;
 
 
-    public Swap(Abobus abobus1, Abobus abobus2)
+    public Swap(HexCell applied_to, Abobus abobus1, Abobus abobus2)
     {
         this.abobus1 = abobus1;
         this.abobus2 = abobus2;
 
-        this.cell1 = abobus1.cell;
-        this.cell2 = abobus2.cell;
+        this.applied_to = applied_to;
 
         game_manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
         
     }
     public HexCell AppliedTo
     {
-        get => cell1;
-        set => cell1 = value;
+        get => applied_to;
+        set => applied_to = value;
     }
-
-    // public HexCell AppliedFrom
-    // {
-    //     get => cell2;
-    //     set => cell2 = value;
-    // }
-
 
     public string DebugMessage()
     {
-        return "Swapping " + abobus1.abobus_name + " with " + abobus2.abobus_name;
+        return "Swaping " + abobus1.abobus_name + " with " + abobus2.abobus_name + " between " + abobus1.cell.hex_coordinates.ToString() + abobus2.cell.hex_coordinates.ToString();
     }
+
 
     public void Invoke()
     {
@@ -50,7 +42,8 @@ public class Swap : IAction
 
     private void SwapPositions()
     {
-        abobus1.MoveToHexCoordinates(cell2.hex_coordinates);
-        abobus2.MoveToHexCoordinates(cell1.hex_coordinates);
+        var tempCoordinates = abobus1.cell.hex_coordinates;
+        abobus1.MoveToHexCoordinates(abobus2.cell.hex_coordinates);
+        abobus2.MoveToHexCoordinates(tempCoordinates);
     }
 }
