a8d1a328b2bec5cfcde447458f35dc2a0a79e2f6
Ekaterina Chabanova <106866835+medochecheck@users.noreply.github.com>
2024-11-12 00:41:38 +0300

    разделила способности леввера

---
+++ Assets/Scripts/Action/AbobiSkill/LevverSkillMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevverSkillMovement : IAction
+{
+    private HexCell applied_to;
+    private Abobus abobus;
+    private GameManager game_manager;
+
+    public LevverSkillMovement(HexCell applied_to, Abobus abobus)
+    {
+        this.applied_to = applied_to;
+        this.abobus = abobus;
+        game_manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+
+    public void Invoke()
+    {
+        Debug.Log("Levver invokes movement skill");
+
+        if (game_manager.selected_abobus == null)
+        {
+            SelectAbobus select_abobus = new SelectAbobus(abobus.cell, abobus);
+            select_abobus.Invoke();
+        }
+
+        List<HexCell> skill_turns = abobus.GetPossibleSkillTurns(applied_to);
+
+        foreach (HexCell cell in skill_turns)
+        {
+            if (HexGrid.GetDistance(abobus.cell, cell) <= 1 && cell.state == HexCell.State.empty)
+            {
+                cell.state = HexCell.State.empty; // Используем состояние "empty" как "highlighted_green"
+                cell.actions.AddLast(new SimpleMovement(cell, abobus));
+                cell.actions.AddLast(new SimpleUnhighlight(cell, skill_turns));
+                cell.actions.AddLast(new UnselectAbobus(cell, abobus));
+            }
+        }
+
+        abobus.cell.actions.AddLast(new SimpleUnhighlight(abobus.cell, skill_turns));
+        abobus.cell.actions.AddLast(new ClearActions<IAction>(abobus.cell, skill_turns));
+        abobus.cell.actions.AddLast(new ReturnHighlights(abobus.cell, abobus));
+        abobus.cell.actions.AddLast(new EndTurn(abobus.cell));
+    }
+}
